b0536eeaab61f6a7131b9ec4bc2f983189c168f4
anitabiaus <123502319+anitabiaus@users.noreply.github.com>
2023-02-28 00:45:18 -0300

    Add files via upload

---
+++ functioncv.js
@@ -0,0 +1,11 @@
+document.getElementById('telefono').addEventListener('click', e => {
+        document.getElementById('datocontacto').textContent = '2213334444';
+});
+
+document.getElementById('mail').addEventListener('click', e => {
+    document.getElementById('datocontacto').textContent = 'mail1gmail.com';
+});
+
+document.getElementById('direccion').addEventListener('click', e => {
+    document.getElementById('datocontacto').textContent = 'Calle 1 y 22';
+});
